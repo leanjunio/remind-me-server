08cd9066a0212118c340a5e49d2791c2fcb808da
Lean Junio <leanjunio@outlook.com>
2021-02-15 12:19:45 -0500

    Add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello world");
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glasses",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
