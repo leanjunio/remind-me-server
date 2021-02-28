adbedd45e3f3eff0fdeba39871a19a22bc920737
leanjunio <leanjunio@live.com>
2021-02-28 00:45:14 -0500

    feat: add Book entity

--- src/entities/Books.ts
+++ src/entities/Books.ts
@@ -1,13 +1,18 @@
+import { ObjectType, ID, Field } from "type-graphql";
 import { Entity, ObjectID, ObjectIdColumn, Column } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class Book {
+  @Field(type => ID)
   @ObjectIdColumn()
   id!: ObjectID;
 
+  @Field()
   @Column()
   title!: string;
 
+  @Field()
   @Column()
   author!: string;
 }
